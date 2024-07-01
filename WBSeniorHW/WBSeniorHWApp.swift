04d28b96b90b9c8b2a66344124bd23c65858df0f
Wtclrsnd <e.imamov@cappasity.com>
2024-07-01 16:29:32 +0300

    Initial Commit

---
+++ WBSeniorHW/WBSeniorHWApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBSeniorHWApp.swift
+//  WBSeniorHW
+//
+//  Created by Emil Shpeklord on 01.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct WBSeniorHWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
